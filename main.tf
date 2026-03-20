a45288db82759c7b304d955a73719e43a407edab
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-03-20 11:06:55 +0530

    changes done

--- main.tf
+++ main.tf
@@ -92,3 +92,9 @@ resource "aws_route_table" "database" {
     var.database_route_table_tags
     ) 
 }
+
+resource "aws_route" "public" {
+  route_table_id            = aws_route_table.public[0].id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
