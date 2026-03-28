5a336795ce62f6e3f081b0cc3b0d724238a8df8e
Yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-03-29 02:05:55 +0530

    changes done

--- data.tf
+++ data.tf
@@ -1,3 +1,15 @@
 data "aws_availability_zones" "az_names" {
   state = "available"
 }
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_route_table" "default" {
+  vpc_id = data.aws_vpc.default.id
+  filter {
+    name   = "association.main"
+    values = ["true"]
+  }
+}
---
+++ peering.tf
@@ -0,0 +1,53 @@
+resource "aws_vpc_peering_connection" "default" {
+    count = var.is_peering_required ? 1 : 0
+  peer_owner_id = var.peer_owner_id
+
+  #accepter
+  peer_vpc_id   = data.aws_vpc.default.id
+
+  #requester
+  vpc_id        = aws_vpc.main.id
+
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.env}-default"
+    }
+  )
+}
+
+resource "aws_route" "public" {
+    count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "private" {
+    count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "database" {
+    count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+
+resource "aws_route" "deafult" {
+    count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.default.id
+  destination_cidr_block    = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
